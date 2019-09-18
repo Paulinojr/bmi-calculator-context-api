b09814c7df7fcba2becd54ea26ebfb7dbc79e002
Paulino Jr <paulino.junior@outlook.com>
2019-09-18 14:01:00 -0300

    Add separate RangeInput component for cleaner code

--- src/components/range-input.js
+++ src/components/range-input.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
+
 export const RangeInput = props => {
-    changeValue (value){
-        this.props.onChange(value)
-    }
+    
     return (
-        <input type="range" min={props.min} max={props.max} step={props.step}  defaultValue={props.defaultValue} onMouseUp={props.handleWeightChange} onTouchEnd={props.handleWeightChange} />
+        <input type="range" min={props.min} max={props.max} step={props.step}  defaultValue={props.defaultValue} onChange={props.onChange} onMouseUp={props.onChange} onTouchEnd={props.onChange} />
     ) 
 }
